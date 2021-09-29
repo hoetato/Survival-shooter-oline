c17c3fcfc719b1c1e97fe32ecec4a65e62f7cd41
caroline <carolinebless94@gmail.com>
2021-09-29 22:34:06 +0700

    commit

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using TMPro;
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+    private static ScoreManager _instance;
+    public static ScoreManager GetInstance()
+    {
+        if (_instance == null)
+        {
+            _instance = new ScoreManager();
+        }
+        return _instance;
+    }
+    TextMeshProUGUI text;
+
+
+    void Awake ()
+    {
+        text = GetComponent<TextMeshProUGUI>();
+        score = 0;
+    }
+
+
+    void Update ()
+    {
+        text.text = "Score: " + score;
+    }
+}
